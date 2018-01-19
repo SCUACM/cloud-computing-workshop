536813365066ae956070cd9e4ad149ef323e381a
thomasvn <thomasnguyen96@gmail.com>
2018-01-19 15:14:03 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# ACM's Cloud Computing Workshop
+
+
+
+#### Notes
+- Set up private key (Account Page --> SSH Keys)
+- Adjust firewall preferences
+
+#### React App
+- (npm install, npm start)
+
+#### Python App
+- sudo apt-get install python-pip
+- https://apps.twitter.com/
+
+
+
+
+
+
+# TwitterFakeNewsDetector
+Gathers tweets and uses machine learning to determine which tweets are fake
+
+# Setting up Tweepy
+ - Create a twitter developer account
+ - Create keys
+ - Install Tweepy (via github and run the setup.py file)
+
+# Setting up MongoDB
+ run commands :
+   - sudo apt-get install mongodb
+   - sudo apt-get update
+   - sudo service mongodb start
+
+# Setting up PyMongo
+ run commands :
+    - sudo pip install pymongo
---
+++ python-app/tweetbot.py
@@ -0,0 +1,74 @@
+import json
+import time
+
+import tweepy
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+
+##################################################
+# Forked from Joey Phan
+##################################################
+
+keywords = 'hello'
+output_file = 'data.txt'
+
+
+# Load keys from keys.json into variables
+filename = 'keys.json'
+if filename:
+    with open(filename, 'r') as f:
+        keys = json.load(f)
+
+consumerKey = keys['consumer_key']
+consumerSecret = keys['consumer_key_secret']
+accessToken = keys['access_token']
+accessSecret = keys['access_token_secret']
+
+
+# Creating Listener class that will gather data from API
+class Listener(StreamListener):
+    def on_data(self, str_data):
+        try:
+            # Data from on_data initially comes in as string. Converts to json
+            data_json = json.loads(str_data)
+
+            # Checking if keywords exist in data
+            if 'id' in data_json and 'text' in data_json and 'user' in data_json:
+                try:
+                    saveFile = open(output_file, 'a')
+                    saveFile.write(str_data)
+                    saveFile.close()
+
+                    # Options of what to print
+                    # print str_data
+                    print "TWEET: " + data_json['text']
+
+                    return True
+                except BaseException, e:
+                    print 'failed on_data: ', str(e)
+                    time.sleep(5)
+            return True
+
+        # Could happen with bad internet... etc other errors
+        except BaseException, e:
+            print 'failed on_data,', str(e)
+            time.sleep(5)
+
+    # Happens when an error occurs - probably through a wrong key
+    def on_error(self, status):
+        print status
+        if status == 420:  # 420 means maxed out on number of requests in a window of time
+            return False
+        time.sleep(30)
+
+
+# Setting Authentication Keys for API
+auth = OAuthHandler(consumerKey, consumerSecret)
+api = tweepy.API(auth)
+auth.set_access_token(accessToken, accessSecret)
+twitterStream = Stream(auth, Listener())
+
+# Gathering tweets with keywords
+twitterStream.filter(track=[keywords])
